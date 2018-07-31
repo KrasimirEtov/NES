6eaaec69d82cdbcc25543948863b31910d13e80a
MESA <4341025+MESA1997@users.noreply.github.com>
2018-07-31 14:58:53 +0300

    skeleton

---
+++ NES/Core/Engine/Factory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NES.Core.Engine
+{
+	public class Factory
+    {
+
+    }
+}
---
+++ NES/Entities/Market/Market.cs
@@ -0,0 +1,12 @@
+﻿using NES.Entities.Assets.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NES.Entities.Market
+{
+    public class Market
+    {
+		private readonly Dictionary<string, List<IAsset>> categories;
+    }
+}
