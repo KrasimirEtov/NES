91215f3e8da027fb1533d64591377c7ac62ba5ad
aniklev <mr.niklev@gmail.com>
2018-08-01 19:56:44 +0300

    Process Invalid command

--- NES/Core/Commands/ProcessCommand.cs
+++ NES/Core/Commands/ProcessCommand.cs
@@ -20,15 +20,26 @@ namespace NES.Core.Commands
                 case "BuyBTC":
                     broker.BuyBTC(command.Amount, user);
                     break;
-                case "BuyETH": break;
-                case "BuyGLD": break;
-                case "BuySLR": break;
-                case "BuyUSD": break;
-                case "BuyEUR": break;
-                case "Register": break;
-                case "Login": break;
-                case "EndDay": break;
-                case "Exit": break;
+                case "BuyETH":
+                    break;
+                case "BuyGLD":
+                    break;
+                case "BuySLR":
+                    break;
+                case "BuyUSD":
+                    break;
+                case "BuyEUR":
+                    break;
+                case "Register":
+                    break;
+                case "Login":
+                    break;
+                case "EndDay":
+                    break;
+                case "Exit":
+                    break;
+                default:
+                    throw new ArgumentException($"{command.Action} is not a valid command.");
             }
         }
     }
