c45bb867be71bfac986173741fa52c590ff496c0
aniklev <mr.niklev@gmail.com>
2018-08-06 19:16:53 +0300

    Engine exit check update

--- NES/Core/Engine/Engine.cs
+++ NES/Core/Engine/Engine.cs
@@ -41,9 +41,9 @@ namespace NES.Core.Engine
 				try
 				{
 					command = Command.Parse(ConsoleManager.ReadLine());
-					ProcessCommand.Process(command, ref this.user, Broker);
 					if (command.Action == "exit") break;
-				}
+                    ProcessCommand.Process(command, ref this.user, Broker);
+                }
 				catch (Exception ex)
 				{
                     Console.ForegroundColor = ConsoleColor.Red;
