abf5c65b96f3b8e49bbc9f37a3ec3797fc123a2f
aniklev <mr.niklev@gmail.com>
2018-08-26 23:06:15 +0300

    More broker unit tests

---
+++ NES.Tests/BrokerTests/EndDayTraiding_Should.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using NES.Core.Engine.Contracts;
+using NES.Entities.Users.Contracts;
+using NES.Tests.Mocks;
+using TradeMarket.Contracts;
+
+namespace NES.Tests.BrokerTests
+{
+    [TestClass]
+    public class EndDayTraiding_Should
+    {
+        [TestMethod]
+        public void Incoke_MarkedUpdatePrices()
+        {
+            var factory = new Mock<IAssetFactory>();
+            var market = new Mock<IMarket>();
+            var user = new Mock<IUser>();
+
+            market.Setup(x => x.UpdatePrices());
+
+            var broker = new BrokerMock(factory.Object, market.Object);
+            broker.EndDayTraiding(user.Object);
+
+            market.Verify(x => x.UpdatePrices(), Times.Once);
+        }
+
+
+        [TestMethod]
+        public void Returns_Success_Message()
+        {
+            string message = "Trading day has ended!";
+            var factory = new Mock<IAssetFactory>();
+            var market = new Mock<IMarket>();
+            var user = new Mock<IUser>();
+
+            market.Setup(x => x.UpdatePrices());
+
+            var broker = new BrokerMock(factory.Object, market.Object);
+            string result = broker.EndDayTraiding(user.Object);
+
+            Assert.AreEqual(message, result);
+        }
+    }
+}
